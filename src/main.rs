fbb43ad668f9203fc160cb11fcd7559d79f5ceef
bitfl0wer <florian@pro-weber.com>
2023-10-27 19:54:10 +0200

    Add serde serialization to WebhookPayload struct

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@ use std::time::Duration;
 
 use chrono::{Local, NaiveDate, NaiveTime};
 use clap::Parser;
-use serde::Deserialize;
-use serde_json::from_str;
+use serde::{Deserialize, Serialize};
+use serde_json::{from_str, to_string};
 
 /// Sends a weekly message via Discord Webhook, containing a random noun.
 #[derive(Debug, Parser)]
@@ -29,6 +29,15 @@ struct WordResponse {
     zero: String,
 }
 
+#[derive(Serialize, Debug)]
+struct WebhookPayload {
+    content: String,
+    embeds: Option<String>,
+    attachments: Vec<String>,
+    username: String,
+    avatar_url: String,
+}
+
 fn main() {
     let args = Args::parse();
     let parsed_time = NaiveTime::from_str(&args.time).expect("Unable to parse Time");
@@ -45,12 +54,13 @@ fn main() {
             next_execution_datetime = (Local::now().date_naive()
                 + chrono::Days::new(args.interval_days))
             .and_time(parsed_time);
-            println!(
-                "Word: {}",
-                get_word().unwrap_or(
-                    "There has been an error with getting todays' random prompt.".to_string()
-                )
-            );
+            send_webhook(
+                &args.webhook_url,
+                &get_word().unwrap_or(
+                    "There has been an error with getting todays' random prompt.".to_string(),
+                ),
+            )
+            .unwrap();
         }
         sleep(Duration::new(5, 0));
     }
@@ -60,4 +70,21 @@ fn get_word() -> Result<String, reqwest::Error> {
     // Blocking here is okay, due to the very low frequency of requests.
     let resp = reqwest::blocking::get("https://random-word-form.repl.co/random/noun")?.text()?;
     Ok(from_str::<WordResponse>(&resp).unwrap().zero)
+}
+
+fn send_webhook(webhook_url: &str, message: &str) -> Result<(), reqwest::Error> {
+    let payload = WebhookPayload {
+        content: message.to_string(),
+        embeds: None,
+        attachments: vec![],
+        username: "Artists' Random Prompt Generator".to_string(),
+        avatar_url:
+            "https://github.com/bitfl0wer/art-prompt-webhook/blob/main/static/icon.png?raw=true"
+                .to_string(),
+    };
+    let client = reqwest::blocking::Client::new();
+    dbg!(to_string(&payload).unwrap());
+    let resp = client.post(webhook_url).header("Content-Type", "application/json").body(to_string(&payload).expect("There has been an error while serializing the Webhook Payload to JSON. Please report this bug at: https://github.com/bitfl0wer/art-prompt-webhook")).send()?;
+    println!("Response: {:?}", resp.status());
+    Ok(())
 }
