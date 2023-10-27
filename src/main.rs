77ab242bb9c3f18f1bed1c25e0b6356a3ed94634
bitfl0wer <florian@pro-weber.com>
2023-10-27 13:25:29 +0200

    Implement get_word()

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use clap::Parser;
+use serde::Deserialize;
+use serde_json::from_str;
+
+#[derive(Debug, Parser)]
+struct Args {
+    #[arg(short, long)]
+    url: String,
+    #[arg(short, long)]
+    time: u16,
+}
+
+#[derive(Deserialize, Debug)]
+struct WordResponse {
+    #[serde(rename = "0")]
+    zero: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("Hello, clap! {:?}", args);
+}
+
+fn get_word() -> Result<String, reqwest::Error> {
+    let resp = reqwest::blocking::get("https://random-word-form.repl.co/random/noun")?.text()?;
+    Ok(from_str::<WordResponse>(&resp).unwrap().zero)
+}
+
+#[test]
+fn test() {
+    println!("{}", get_word().unwrap());
 }
